f4a7c3f9f07717ef672d8b3327c4ed28f96a5e28
Imonikai <108214765+Imonikai@users.noreply.github.com>
2022-10-06 23:09:30 +0900

    空行後の改行オプションを削除

--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
 import os
 import PySimpleGUI as sg
 
-def cvt_webnovel(from_filename, to_filename, del_indent, enter_empty_line):
+def cvt_webnovel(from_filename, to_filename, del_indent):
     
     with open(from_filename, 'r', encoding='utf-8') as file:
         lines = file.read().splitlines()
 
     with open(to_filename, 'w', encoding='utf-8') as file:
         for line in lines:
-            if( line != '' ):
+            if(line != ''):
                 line = line[0].replace(' ', '　') + line[1:]
                 if( line[0] == '　' and del_indent):
                     line = line[1:]
                 file.write(f'{line}\n\n')
-            elif(enter_empty_line):
+            else:
                 file.write('\n')
 
 
+
 layout = [
-    [sg.Checkbox('字下げ削除', default=False, key='del_indent'), sg.Checkbox('空行の後も改行', default=False, key='enter_empty_line')],
+    [sg.Checkbox('字下げ削除', default=False, key='del_indent')],
     [sg.Text('変換元'), sg.InputText(), sg.FileBrowse(file_types=(('txtファイル', '*.txt'),), key='from')],
     [sg.Text('変換先'), sg.InputText(), sg.FileSaveAs(file_types=(('txt', '*.txt'),), key='to')],
     [sg.Submit('変換'), sg.Cancel('やめる')],
@@ -33,7 +34,7 @@ while True:
         break
 
     if(values['from'] != '' and values['to'] != '' and event == '変換'):
-        cvt_webnovel(values['from'], values['to'], values['del_indent'], values['enter_empty_line'])
+        cvt_webnovel(values['from'], values['to'], values['del_indent'])
         sg.popup_ok('変換しました')
 
 window.close()
